37087295688f34b21072fb9c7fb43ac5e5fe39cb
Cameron Chunn <cameronchunn@gmail.com>
2023-05-11 11:13:52 -0500

    Disable internal redis if not using it in Taky

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,30 +1,29 @@
 #!/bin/bash
 
-echo "Starting REDIS..."
-redis-server --daemonize yes
-
-
 echo "Setting up taky environment..."
 
-mkdir -p /takdata
-mkdir -p /takdata/log
-mkdir -p /takdata/ssl
-
-touch /var/taky/taky-mgmt.sock
+sudo mkdir -p /takdata/log
+sudo mkdir -p /takdata/ssl
 
 # check if /takdata/taky.conf exists, and if not, copy the example config file
 if [ ! -f /takdata/taky.conf ]; then
-    # copy files from /usr/local/tomcat/webapps/ROOT to /takdata
     cp -r /app/taky.conf.example /takdata/taky.conf
+fi
+
+# check /takdata/taky.conf for the REDIS value
+REDIS=$(grep redis /takdata/taky.conf)
+if [[ "$REDIS" == *"True"* ]]; then
+  echo "Starting internal redis server..."
+  redis-server --daemonize yes
+else 
+  echo "Internal redis server disabled. $redis"
 fi
 
 echo "Getting public IP address..."
 PUBLIC_IP=$(curl -s ifconfig.me)
-echo "Public IP address is $PUBLIC_IP, Updating taky.conf..."
 
+echo "Public IP address is $PUBLIC_IP, Updating taky.conf..."
 # replace the public_ip value in taky.conf with the public IP address
 sed -i "s/\(public_ip *= *\).*/\1$PUBLIC_IP/" /takdata/taky.conf
-
-
 
 taky -l debug -c /takdata/taky.conf
